4302ee997e5736077ccfe257ae57a2817b66fc14
Dostonbek Ahmadaliev <ahmadalievd384@gmail.com>
2025-11-01 12:20:41 +0900

    feat:DAVE/ integration env variables

--- apps/timory-batch/src/timory-batch.module.ts
+++ apps/timory-batch/src/timory-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TimoryBatchController } from './timory-batch.controller';
 import { TimoryBatchService } from './timory-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()], // we can use .env file
   controllers: [TimoryBatchController],
   providers: [TimoryBatchService],
 })
