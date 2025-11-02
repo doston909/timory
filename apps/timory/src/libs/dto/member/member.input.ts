7c469511d71ffed2a758a3ab073bd96a207a4fd5
Dostonbek Ahmadaliev <ahmadalievd384@gmail.com>
2025-11-02 10:11:00 +0900

    feat:DAVE/ integrate validation pipes logic

---
+++ apps/timory/src/libs/dto/member/member.input.ts
@@ -0,0 +1,42 @@
+import { Field, InputType } from "@nestjs/graphql";
+import { IsNotEmpty, IsOptional, Length } from "class-validator";
+import { MemberAuthType, MemberType } from "../../enums/member.enum";
+
+
+@InputType()
+export class MemberInput {
+    @IsNotEmpty()           // bo'sh bo'lmaslik sharti
+    @Length(3, 12)          // uzunligi 3-12 orasida bo'lishi
+    @Field(() => String)    // string qaytarishi sharti
+    memberNick: string;
+
+    @IsNotEmpty()
+    @Length(5, 12)
+    @Field(() => String)
+    memberPassword: string;
+
+    @IsNotEmpty()
+    @Field(() => String)
+    memberPhone: string;
+
+    @IsOptional()                                       // bo'lishi yoki bo'lmasligi ham mumkin
+    @Field(() => MemberType, { nullable: true })        // MemberType shaklida data qaytarishi va bo'sh bo'lishi mumkinligi sharti
+    memberType?: MemberType;                            // memberType type MemberType bo'lishi sharti
+
+    @IsOptional()                                       // bo'lishi yoki bo'lmasligi ham mumkin
+    @Field(() => MemberAuthType, { nullable: true })    // MemberAuthType shaklida data qaytarishi va bo'sh bo'lishi mumkinligi sharti
+    memberAuthType?: MemberAuthType;                    // memberAuthType type MemberAuthType bo'lishi sharti
+}
+
+@InputType() 
+export class LoginInput {
+    @IsNotEmpty()
+    @Length(3, 12)
+    @Field(() => String)
+    memberNick: string;
+
+    @IsNotEmpty()
+    @Length(5, 12)
+    @Field(() => String)
+    memberPassword: string;
+}
